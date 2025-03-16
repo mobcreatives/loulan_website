ac88dc9401d72151cab5715ad7986ab4afe9d9db
if1eight0sty <ma18yan@gmail.com>
2025-03-16 22:06:05 +0545

    menu completed

---
+++ src/app/types.ts
@@ -0,0 +1,10 @@
+import { Dispatch, JSX, SetStateAction } from "react";
+
+export type TMenuEnum = "desert" | "steak" | "coffee" | "burger";
+
+export type TMenuButtonPros = {
+  activeTab: TMenuEnum;
+  setActiveTab: Dispatch<SetStateAction<TMenuEnum>>;
+  text: TMenuEnum;
+  icon: JSX.Element;
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,7 @@ export * from "./floating-navbar";
 export * from "./text-with-line";
 
 export * from "./icons/fill-star-icon-component";
+export * from "./icons/desert-icon-component";
+export * from "./icons/steak-icon-component";
+export * from "./icons/coffee-icon-component";
+export * from "./icons/burger-icon-component";
