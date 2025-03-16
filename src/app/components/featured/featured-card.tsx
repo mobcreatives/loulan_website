bec6963dcc5352ea0a181b740915fe3605dd5ada
if1eight0sty <ma18yan@gmail.com>
2025-03-17 00:10:10 +0545

    featured completed

---
+++ src/app/components/featured/featured-card.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { TFeaturedCardPros } from "./types";
+
+export default function FeaturedCard({
+  description,
+  price,
+  title,
+  image,
+}: Readonly<TFeaturedCardPros>) {
+  return (
+    <div
+      className="font-fredoka rounded-[20px] border-2 border-[#121A1D] px-10 pt-5 pb-10 h-60 w-120 lg:w-100 xl:w-110 relative bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: `url(${image})`,
+      }}
+    >
+      <h4 className="text-[clamp(2.25rem,2.1266rem+0.5195vw,2.75rem)] font-bold">
+        {title}
+      </h4>
+      <p className="max-w-[25ch]">{description}</p>
+      <div className="absolute bg-primary size-25 rounded-full left-10 -bottom-5 flex items-center justify-center flex-col -space-y-3">
+        <p className="text-[#F3274C] font-bold text-[clamp(1.25rem,1.1883rem+0.2597vw,1.5rem)]">
+          Rs {price}
+        </p>
+        <p className="text-black">person</p>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "./components/hero-section";
 import RateYourExperience from "./components/rate-your-experience";
 import Menu from "./components/menu/menu";
 import Special from "./components/special/special";
+import Featured from "./components/featured/featured";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <RateYourExperience />
       <Menu />
       <Special />
+      <Featured />
     </main>
   );
 }
