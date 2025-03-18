8a615d4b851d6c2a92bdab6d8a770e986fa2cc8e
if1eight0sty <ma18yan@gmail.com>
2025-03-18 22:52:26 +0545

    made the reservation section of home page responnsive

--- src/app/components/reservation/reservation.tsx
+++ src/app/components/reservation/reservation.tsx
@@ -6,7 +6,7 @@ export default function Reservation() {
   return (
     <section className="flex justify-center px-6 sm:px-10 md:px-16 lg:px-40 2xl:px-44 py-12">
       <div className="max-w-[1200px] text-white w-full">
-        <div className="flex justify-between">
+        <div className="flex flex-col items-center lg:flex-row lg:justify-between gap-8">
           <div className="text-center">
             <div className="uppercase text-[clamp(1.875rem,1.7825rem+0.3896vw,2.25rem)] font-bold -space-y-2">
               <h4 className="tracking-wide">Reserve</h4>
@@ -17,8 +17,8 @@ export default function Reservation() {
             </div>
             <p className="mt-4">Discover our New Menu!</p>
           </div>
-          <form className="font-epilogue space-y-6 text-[clamp(0.875rem,0.85rem+0.125vw,1rem)] w-200">
-            <div className="grid grid-cols-2 gap-4 ">
+          <form className="font-epilogue space-y-6 text-[clamp(0.875rem,0.85rem+0.125vw,1rem)] w-100 md:w-150 xl:w-200">
+            <div className="grid md:grid-cols-2 gap-4">
               <input
                 type="text"
                 name="number-of-guest"
@@ -26,7 +26,7 @@ export default function Reservation() {
                 placeholder="No. of Guests"
                 className="bg-white h-12 rounded-[8px] placeholder-[#555555] px-4 font-semibold focus:outline-[#FFD700]"
               />
-              <div className="grid grid-cols-2 gap-4 ">
+              <div className="grid md:grid-cols-2 gap-4 ">
                 <input
                   type="text"
                   name="date"
@@ -57,8 +57,8 @@ export default function Reservation() {
                 className="bg-white h-12 rounded-[8px] placeholder-[#555555] px-4 font-semibold focus:outline-[#FFD700]"
               />
             </div>
-            <div className="flex justify-end px-4">
-              <ButtonWithBorder text="Submit" />
+            <div className="flex justify-end lg:px-4">
+              <ButtonWithBorder text="Submit" className="w-full lg:w-fit" />
             </div>
           </form>
         </div>
