93401d670f69f6ce713b06a325401a547b70f916
Ranjit Budhathoki <99970842+ranjitbudhathoki@users.noreply.github.com>
2025-03-31 12:07:12 +0545

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://loulan-server.mobbipin18.workers.dev/";
+export const BASE_URL = "https://loulan-server.loulan.workers.dev/";
 // export const BASE_URL = "http://localhost:5000/";
