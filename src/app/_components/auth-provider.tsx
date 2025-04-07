b32e008825322bdef7742357d30c33a90fe920a0
if1eight0sty <ma18yan@gmail.com>
2025-04-07 23:55:27 +0545

    made the dashboard protected

---
+++ src/app/_components/auth-provider.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useLocalStorage } from "@/hooks/use-local-storage";
+import { CONSTANTS } from "@/config/constants";
+import { APP_ROUTES } from "@/config/routes";
+
+export default function AuthProvider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const router = useRouter();
+  const { getItem } = useLocalStorage();
+  const token = getItem(CONSTANTS.TOKEN);
+
+  useEffect(() => {
+    if (!token) {
+      router.push(APP_ROUTES.LOGIN);
+    }
+  }, [token, router]);
+  
+  return <>{children}</>;
+}
