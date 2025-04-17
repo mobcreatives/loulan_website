eb704a52f38756fa883e0962988e4668cf4072b9
Ranjit Budhathoki <ranjitbudhathoki07@gmail.com>
2025-04-17 21:42:50 +0545

    fix1"

--- src/app/(client)/menu/page.tsx
+++ src/app/(client)/menu/page.tsx
@@ -13,12 +13,8 @@ export default function MenuPage() {
       <section className="px-6 sm:px-10 md:px-16 lg:px-28 xl:px-36 2xl:px-44 py-10 space-y-14">
         <div className="space-y-3 flex flex-col items-center">
           <TextWithLine
-            text="Menu"
-            className="before:w-[100%] before:h-[1px] before:-bottom-0.5"
-          />
-          <TextWithLine
             text="Discover Menu"
-            className="font-fredoka text-[clamp(2.125rem,2.0325rem+0.3896vw,2.5rem)] font-bold before:w-[170px] before:h-[5px] before:-bottom-1"
+            className="font-fredoka text-white text-[clamp(2.125rem,2.0325rem+0.3896vw,2.5rem)] font-bold before:w-[170px] before:h-[5px] before:-bottom-1"
           />
         </div>
         <FoodList categoryId={categoryId ? parseInt(categoryId) : undefined} />
